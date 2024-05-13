cc62d3f04813170afed997c295d1cf4b12ad7e0e
jong-un <36259838+zwong91@users.noreply.github.com>
2024-05-13 16:34:36 +0800

    Remove pledge_examples scripts

--- scripts/pledge_examples/power2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-## Challenge Rsa2048 keys
-sleep 5
-unc extensions create-challenge-rsa 79d96a47f387ae8f8e92a7f5b42e75d86c31c680581ad77cef1115a5b76b6e3b use-file ~/.unc/keys/challenge3.json without-init-call network-config custom sign-with-access-key-file ~/.unc/Unc3/signer_key.json send
-sleep 5
-unc extensions create-challenge-rsa 79d96a47f387ae8f8e92a7f5b42e75d86c31c680581ad77cef1115a5b76b6e3b use-file ~/.unc/keys/challenge4.json without-init-call network-config custom sign-with-access-key-file ~/.unc/Unc3/signer_key.json send
--- scripts/pledge_examples/register_rsa.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-## Register Rsa2048 keys
-sleep 5
-unc extensions register-rsa-keys unc use-file ~/.unc/keys/batch_register_rsa1.json with-init-call network-config custom sign-with-access-key-file ~/.unc/keys/unc.json send
-sleep 5
-unc extensions register-rsa-keys unc use-file ~/.unc/keys/batch_register_rsa2.json with-init-call network-config custom sign-with-access-key-file ~/.unc/keys/unc.json send
