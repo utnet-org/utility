fee3a8d876447b1871f13eec8e84267263ef098d
jong-un <36259838+zwong91@users.noreply.github.com>
2024-05-21 17:43:14 +0800

    chore: Remove Fedora 39 and Fedora 40 build support

--- scripts/binaries/build_all.sh
+++ scripts/binaries/build_all.sh
@@ -3,6 +3,6 @@ export ENGINE="${ENGINE:-docker}"
 export ARCH="${ARCH:-x64}"
 export ZIP="true"
 
-for distro in ubuntu-2004 ubuntu-2204 ubuntu-2404 fedora-39 fedora-40 debian-11 debian-12 arch; do
+for distro in ubuntu-2004 ubuntu-2204 ubuntu-2404 debian-11 debian-12 arch; do
     DISTRO=${distro} ./scripts/binaries/build.sh
 done
